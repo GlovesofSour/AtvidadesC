47b330a1fe68c8724da85a0e72f070394d0ecc32
Lucas Paulino <112363220+GlovesofSour@users.noreply.github.com>
2023-03-25 10:42:48 -0300

    Update At2ex01.c

--- At2ex01.c
+++ At2ex01.c
@@ -1,4 +1,4 @@
-/*1. Faça um programa que leia dois números e mostre qual deles é o maior.*/
+}/*1. Faça um programa que leia dois números e mostre qual deles é o maior.*/
 
 #include <stdio.h>
 
@@ -6,17 +6,21 @@ int main(){
 
     int n1, n2;
 
+    printf("-----------------------------\n");
+    printf("Vericador de maior numero\n");
+    printf("-----------------------------\n");
     printf("Digite o primeiro numero: ");
     scanf("%d", &n1);
     printf("Digite o segundo numero: ");
     scanf("%d", &n2);
+    printf("-----------------------------\n");
 
     if(n1 > n2){
-        printf("O numero %d eh o maior numero", n1);
+        printf("O numero %d eh o maior numero\n", n1);
+    }else{
+        printf("O numero %d eh o maior numero\n", n2);
     }
-    if(n2 > n1){
-        printf("O numero %d eh o maior numero", n2);
-    }
+    printf("-----------------------------\n");
 
     return 0;
 }
